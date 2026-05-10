75442b656e8fa75bb3e004b872c01a464e8f3086
Naveen <naveengeethlakdinu@gmail.com>
2026-05-10 21:40:20 +0530

    navigate to the loan deatils

--- app/src/main/java/com/example/smartfinancialmanagement/RegisterActivity.java
+++ app/src/main/java/com/example/smartfinancialmanagement/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.smartfinancialmanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CheckBox;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -9,5 +11,13 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        CheckBox loanCheckbox = findViewById(R.id.checkLoan);
+        loanCheckbox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                Intent intent = new Intent(RegisterActivity.this, LoanDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
