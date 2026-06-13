583a0f543f38528e2cc5225f762c7b69f23ebc3d
Sahanya <dulanisahanya227@gmail.com>
2026-06-13 22:39:21 +0530

    feat: Add Student Dashboard UI and navigation resources

---
+++ app/src/main/java/com/example/smartfinancialmanagement/StudentDashboardActivity.java
@@ -0,0 +1,12 @@
+package com.example.smartfinancialmanagement;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StudentDashboardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_dashboard);
+    }
+}
